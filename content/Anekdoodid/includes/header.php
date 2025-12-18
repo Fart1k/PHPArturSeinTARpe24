eedf05acfc66dd5e9c2f7f9a8711a5c620979723
Fart1k <seinartur@gmail.com>
2025-12-18 17:53:02 +0200

    Add files via upload

---
+++ content/Anekdoodid/includes/header.php
@@ -0,0 +1,9 @@
+<header>
+  <nav>
+    <ul class="menu">
+      <li><a href="../index.php">Anekdoot 1</a></li>
+      <li><a href="../anekdoot2.php">Anekdoot 2</a></li>
+      <li><a href="../anekdoot3.php">Anekdoot 3</a></li>
+    </ul>
+  </nav>
+</header>
